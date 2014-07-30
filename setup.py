e43c88fd132682f0253efe6411efdf5383adffda
carlio <git@carlcrowder.com>
2014-07-30 14:56:36 +0200

    Marking the package as not-zip-safe

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 
-_version = '0.2'
+_version = '0.2.1'
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
 _short_description = "pylint-common is a Pylint plugin to improve Pylint error analysis of the" \
@@ -29,8 +29,9 @@ setup(
     description=_short_description,
     version=_version,
     packages=_packages,
-    install_requires=['pylint>=1.0', 'astroid>=1.0', 'pylint-plugin-utils>=0.1'],
+    install_requires=['pylint>=1.0', 'astroid>=1.0', 'pylint-plugin-utils>=0.2.1'],
     license='GPLv2',
     classifiers=_classifiers,
-    keywords='pylint stdlib plugin'
+    keywords='pylint stdlib plugin',
+    zip_safe=False  # see https://github.com/landscapeio/prospector/issues/18#issuecomment-49857277
 )
