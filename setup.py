a1d8559e688c2e7bb1486e672025983386111916
carlio <git@carlcrowder.com>
2017-03-15 14:25:21 +0800

    Adding python 3.6 as a supported python version and merging in fix to include the licence in the created package; also bumped requirement on pylint-plugin-utils just for completeness

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import find_packages, setup
 
 
-_version = '0.2.3'
+_version = '0.2.4'
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
 _short_description = ("pylint-common is a Pylint plugin to improve Pylint "
@@ -20,6 +20,7 @@ _classifiers = (
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
 )
 
 
@@ -29,12 +30,12 @@ if sys.version_info < (2, 7):
         'pylint>=1.0,<1.4',
         'astroid>=1.0,<1.3.0',
         'logilab-common>=0.60.0,<0.63',
-        'pylint-plugin-utils>=0.2.4',
+        'pylint-plugin-utils>=0.2.5',
     ]
 else:
     _install_requires = [
         'pylint>=1.0',
-        'pylint-plugin-utils>=0.2.4',
+        'pylint-plugin-utils>=0.2.5',
     ]
 
 
